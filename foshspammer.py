8a668793e00fc878d32eb6ec62b0b3dc7344749c
Skyman-Coder <103896310+Skyman-Coder@users.noreply.github.com>
2022-04-17 11:17:09 -0700

    Update foshspammer.py

--- foshspammer.py
+++ foshspammer.py
@@ -6,5 +6,6 @@ time.sleep(10)
 txt = open('text.txt', 'r')
 
 for i in txt:
-    pg.write("To Ek %s Haste"%(i))
+    pg.write(i)
     pg.press('Enter')
+print('Fosh Ha Ba Movafageiat Chop Shodand!')
